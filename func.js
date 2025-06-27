1467e831c1cd5dcb511b88cbabcfe6d27728aca2
discreett <vdinh2999@gmail.com>
2025-06-27 02:04:57 -0500

    Added visualizer

--- func.js
+++ func.js
@@ -1,6 +1,57 @@
-let camera = document.getElementById('camera-stream')
-let startVid = document.getElementById('start-camera')
-let endVid = document.getElementById('stop-camera')
+const camera = document.getElementById('camera-stream')
+const startVid = document.getElementById('start-camera')
+const endVid = document.getElementById('stop-camera')
+
+
+const visualizer = document.getElementById('visualizer');
+
+const context = new AudioContext()
+const analyzerNode = new AnalyserNode(context, { fftsize: 256 })
+
+setupContext()
+drawVisualizer()
+
+async function setupContext() {
+    const instrument = await getInstrument()
+    if (context.state === 'suspended') {
+        await context.resume()
+    }
+    const source = context.createMediaStreamSource()
+    source
+        .connect(analyzerNode)
+        .connect(context.destination)
+}
+
+function getInstrument() {
+    return navigator.mediaDevices.getUserMedia({
+        audio: {
+            echoCancellation: false,
+            autoGainControl: false,
+            noiseSuppression: false,
+            latency: 0
+        }
+    })
+}
+
+function drawVisualizer() {
+    requestAnimationFrame(drawVisualizer)
+    const bufferLength = analyzerNode.frequencyBinCount
+    const dataArray = new Uint8Array(bufferLength)
+    analyzerNode.getByteFrequencyData(dataArray)
+    const width = visualizer.width
+    const height = visualizer.height
+    const barWidth = width / bufferLength
+    const canvasContext = visualizer.getContext('2d')
+    canvasContext.clearRect(0, 0, width, height)
+
+    dataArray.forEach((item, index) => {
+        const y = item / 255 * height / 2
+        const x = barWidth * index
+
+        canvasContext.fillStyle = 'rgb(0,0,0)'
+        canvasContext.fillRect(x, height - y, barWidth, y)
+    })
+}
 
 let mediaStream;
 
@@ -39,4 +90,4 @@ endVid.addEventListener('click', async () => {
     camera.srcObject = null;
     startVid.disabled = false;
     endVid.disabled = true;
-});
+}); 
